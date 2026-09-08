ffb91fbce17503033fc11fb2df04ba05366da6cd
Preeti Kumari <preetikumari88873@gmail.com>
2026-09-08 14:42:54 +0530

    Implemented officer override system

---
+++ engine/officer_override.py
@@ -0,0 +1,8 @@
+decision = {
+    "violation_id": "V-1001",
+    "system_decision": "NON_COMPLIANT",
+    "officer_override": "COMPLIANT",
+    "reason": "MRP visible in image but OCR failed"
+}
+
+print(decision)
